29a538eea880e4e089b6874d5e411549fae41258
JP-Mbaka <mbakajohnpaul@gmail.com>
2025-04-14 07:32:57 +0100

    test

--- app.py
+++ app.py
@@ -11,6 +11,9 @@ import asyncio
 from dict import Predict
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBClassifier
 import joblib 
 
 
