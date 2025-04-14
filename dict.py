1fd7de2dcad601d94e1dc2e18454a8d4397c7048
JP-Mbaka <mbakajohnpaul@gmail.com>
2025-04-14 07:11:47 +0100

    Predictions

--- dict.py
+++ dict.py
@@ -5,4 +5,11 @@
 from pydantic import BaseModel
 
 class Predict(BaseModel):
-    performance: float
+    department: int
+    year: int
+    difficultyEng: int
+    difficultyComp: int
+    difficultyStat: int
+    qualityEng: int
+    qualityComp: int
+    qualityStat: int
